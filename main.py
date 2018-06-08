f2e1b8b7052904ca7495f8348b8b8832019ce06b
mufeed <mufeedm533@gmail.com>
2018-06-08 17:30:35 +0530

    removed unncessary functions and added comments

--- main.py
+++ main.py
@@ -3,27 +3,20 @@ from services.sentence_tokenizer import SentenceTokenizer
 
 app: Flask = Flask(__name__)
 
-
-@app.route("/home")
-def home():
-    nltk.download()
-    return "hai"
-
-
-@app.route("/test", methods=['POST', 'GET'])
-def test():
-    return "%s" % request.form.get('name')
-
-
+"""
+Method to get input text from user and convert it into sentences
+"""
 @app.route("/sentenceTokenize", methods=['POST'])
 def sentence_tokenize():
     text = request.form.get('text')
-    print(text)
     sentence_tokenizer_object = SentenceTokenizer(text)
     tokenized_words = sentence_tokenizer_object.tokenizer()
     return render_template("SentenceTokenizer.html", words = tokenized_words)
 
 
+"""
+Method to get input from user and split teh sentences into words
+"""
 @app.route("/wordTokenize", methods=['POST'])
 def word_tokenize():
     pass
